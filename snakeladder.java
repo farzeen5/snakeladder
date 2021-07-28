93d14d7a26cae47884babc94f905de823afe6fd5
Farzeen <farzeenkhan1412@gmail.com>
2021-07-28 18:30:10 +0530

    uc3

--- snakeladder.java
+++ snakeladder.java
@@ -4,19 +4,44 @@ import java.util.Random;
 
 class Game{
 	int position = 0;
-	public static int Rolldice()
-	{
-		Random r = new Random();
-		int num = r.nextInt(6)+1;
-		return num;
-	}
 }
+	
 public class snakeladder {
 
 	public static void main(String[] args) {
 		Game player1 = new Game();
-		System.out.println(player1.Rolldice());
+		int dicenumber= dice();
+		int option= condition();
+		int move = dicenumber*option;
+		player1.position=player1.position+move;
+		System.out.println("position of player1 is : " +player1.position);
 
 	}
-
+	
+		private static int dice() {
+		Random r = new Random();
+		int num = r.nextInt(6)+1;
+		return num;
+	}
+		private static int condition() {
+			Random r = new Random();
+			int flag = r.nextInt(3);
+			int temp;
+			if(flag==0)
+			{
+				temp=0;
+				System.out.println("no play");
+			}
+			else if(flag==1) {
+				temp=1;
+				System.out.println("ladder");
+			}
+			else {
+				temp=-1;
+				System.out.println("snake");
+			}
+			return temp;
+			
+		}
 }
+
