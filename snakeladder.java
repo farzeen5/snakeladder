dffe47b0a791cdba773e4589152b47b74c32fed7
Farzeen <farzeenkhan1412@gmail.com>
2021-07-28 16:12:06 +0530

    uc2

--- snakeladder.java
+++ snakeladder.java
@@ -1,12 +1,21 @@
 package snakeladder;
+
+import java.util.Random;
+
 class Game{
 	int position = 0;
+	public static int Rolldice()
+	{
+		Random r = new Random();
+		int num = r.nextInt(6)+1;
+		return num;
+	}
 }
 public class snakeladder {
 
 	public static void main(String[] args) {
 		Game player1 = new Game();
-		System.out.println(player1.position);
+		System.out.println(player1.Rolldice());
 
 	}
 
