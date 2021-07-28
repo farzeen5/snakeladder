89513a3ccb2d439ae057014ebaf841ec72687ba6
Farzeen <farzeenkhan1412@gmail.com>
2021-07-28 16:02:34 +0530

    firstcommit

---
+++ snakeladder.java
@@ -0,0 +1,13 @@
+package snakeladder;
+class Game{
+	int position = 0;
+}
+public class snakeladder {
+
+	public static void main(String[] args) {
+		Game player1 = new Game();
+		System.out.println(player1.position);
+
+	}
+
+}
